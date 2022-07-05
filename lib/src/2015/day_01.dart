c149f8e8daf5bb160a07a1b9a6f7d490d98cebab
Tim Austin <tim@neenjaw.com>
2022-07-04 21:31:27 -0600

    day 01 - part 2

--- lib/src/2015/day_01.dart
+++ lib/src/2015/day_01.dart
@@ -15,9 +15,36 @@ class Day01 {
 
     return result.first;
   }
+
+  Future<int> part2(String input) async {
+    final transformer = StreamTransformer.fromHandlers(
+        handleData: (String value, EventSink<int> sink) {
+      sink.add(find(value));
+    });
+
+    final result = await Reader().readInput<int>(input, transformer);
+
+    return result.first;
+  }
 }
 
 int tally(String s) {
   return s.split('').fold(
       0, (balance, bracket) => (bracket == '(') ? balance + 1 : balance - 1);
+}
+
+int find(String s) {
+  var floor = 0;
+  for (var entry in s.split('').asMap().entries) {
+    if (entry.value == '(') {
+      floor += 1;
+    } else {
+      floor -= 1;
+    }
+
+    if (floor == -1) {
+      return entry.key + 1;
+    }
+  }
+  throw 'did not reach basement';
 }
--- test/advent_of_dart_2015_test.dart
+++ test/advent_of_dart_2015_test.dart
@@ -2,16 +2,17 @@ import 'package:advent_of_dart/advent_of_dart_2015.dart';
 import 'package:test/test.dart';
 
 void main() {
-  group('A group of tests', () {
+  group('Day 01', () {
     final day01 = Day01();
 
-    setUp(() {
-      // Additional setup goes here.
-    });
-
-    test('First Test', () {
+    test('part 1', () {
       expect(
           day01.part1('./test/2015/day_01_input.txt'), completion(equals(280)));
+    });
+
+    test('part 2', () {
+      expect(day01.part2('./test/2015/day_01_input.txt'),
+          completion(equals(1797)));
     });
   });
 }
